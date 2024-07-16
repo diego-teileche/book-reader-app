6067b25ed95c69cbc8e35e6b609e73219cc4cefd
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-16 13:40:01 -0300

    Setup done

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return <header style={headerStyle}></header>
+}
+
+const headerStyle = {
+	display: "flex",
+	justifyContent: "space-between",
+	alignItems: "center",
+	padding: "1rem 1re 1rem 0",
+	color: "#f0f0f0",
+}
+
+export default Header
