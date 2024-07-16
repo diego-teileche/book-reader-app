13a7370aaa8d1638d2ca578c31660988b4057568
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-16 18:18:38 -0300

    Books Section | Book Card
    - BookCard created and added
    - Styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Sidebar from "@/components/Sidebar"
 import { books } from "../constants/mockData"
 
 import styles from "./page.module.css"
+import BookCard from "@/components/BookCard"
 
 export default function Home() {
 	return (
@@ -23,22 +24,30 @@ export default function Home() {
 							transition={{ type: "spring", damping: 50, mass: 0.75 }}
 							initial={{ opacity: 0, x: 200 }}
 							animate={{ opacity: 1, x: 0 }}
+							style={{ marginLeft: "30px", paddingLeft: "1rem" }}
 						>
 							All Books
 						</motion.h1>
 
-						<ul>
+						<ul className={styles.ulStyle}>
 							{books.map((book, i) => (
 								<motion.li
 									key={i}
-									whileHover={{ scale: 1.1 }}
 									whileTap={{ scale: 0.9 }}
 									transition={{ type: "spring", damping: 50, mass: 0.75 }}
 									initial={{ opacity: 0, x: 200 * (i + 1) }}
 									animate={{ opacity: 1, x: 0 }}
 								>
-									<a href={`/book/${book.id}`}>
-										<h4>{book.title}</h4>
+									<a
+										href={`/book/${book.id}`}
+										style={{ textDecoration: "none" }}
+									>
+										<BookCard
+											title={book.title}
+											coverImage={book.image}
+											description={book.description}
+											onClick={() => {}}
+										/>
 									</a>
 								</motion.li>
 							))}
--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,5 +1,61 @@
-const BookCard = () => {
-	return <div>BookCard</div>
+"use client"
+interface BookCardProps {
+	title: string
+	description: string
+	coverImage: string
+	onClick: any
+}
+
+const BookCard: React.FC<BookCardProps> = ({
+	title,
+	description,
+	coverImage,
+	onClick,
+}) => {
+	return (
+		<div style={cardStyle} onClick={onClick}>
+			<img src={coverImage} style={imageStyle} alt={title} />
+
+			<div style={contentStyle}>
+				<h3 style={titleStyle}>{title}</h3>
+
+				<p style={descriptionStyle}>{description}</p>
+			</div>
+		</div>
+	)
+}
+
+const cardStyle = {
+	width: "200px",
+	padding: "1rem",
+	borderRadius: "5px",
+	backgroundColor: "#151515",
+	boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.1)",
+	cursor: "pointer",
+	transition: "transform 0.3s ease",
+	marginBottom: "1.5rem",
+	marginRight: "1.6rem",
+}
+
+const imageStyle = {
+	width: "100%",
+	aspectRatio: 1,
+	borderRadius: "10px",
+}
+
+const contentStyle = {
+	marginTop: "1rem",
+}
+
+const titleStyle = {
+	fontSize: "1.2rem",
+	marginBottom: "0.5rem",
+	color: "#f0f0f0",
+}
+
+const descriptionStyle = {
+	fontSize: "0.8rem",
+	color: "#a0a0a0",
 }
 
 export default BookCard
